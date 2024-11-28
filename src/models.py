161e4d5858659d2344e2d4c22407350267125707
Nicolás <78237583+SirNico6@users.noreply.github.com>
2024-11-28 19:57:14 +0000

    First commit, seems good

--- src/models.py
+++ src/models.py
@@ -4,29 +4,44 @@ from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
+from sqlalchemy import DateTime
+
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'USER'
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String(250), nullable=False)
+    last_name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+    subscribe_date = Column(DateTime)
+
+class Planet(Base):
+    __tablename__ = 'PLANET'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    population = Column(Integer)
+    climate = Column(String(50), nullable=True)
+    terrain = Column(String(50), nullable=True)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class People(Base):
+    __tablename__ = 'PEOPLE'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
-
-    def to_dict(self):
-        return {}
+    name = Column(String(200))
+    age = Column(Integer)
+    birthday_year = Column(String(50), nullable=True)
+    
+class Favorite(Base):
+    __tablename__ = 'FAVOURITE'
+    id = Column(Integer, primary_key=True)
+    people_id = Column(Integer, ForeignKey('PEOPLE.id'))
+    people = relationship(People)
+    user_id = Column(Integer, ForeignKey('USER.id'))
+    user = relationship(User)
+    planet_id = Column(Integer, ForeignKey('PLANET.id'))
+    planet = relationship(Planet)
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
